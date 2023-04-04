43b7c9d5271f39318ce9ec793655c8cd8e8934e2
Anna Novozhilova <anny-nov@ya.ru>
2023-04-04 16:59:41 +0300

    Version 3.1
    Log list and log list filter were added

--- main_code/ComputerInfoWindow.py
+++ main_code/ComputerInfoWindow.py
@@ -1,4 +1,4 @@
-import os
+from datetime import datetime
 
 from PyQt6.QtCore import QSize, Qt, QTimer
 from PyQt6.QtWidgets import QApplication, \
@@ -6,12 +6,9 @@ from PyQt6.QtWidgets import QApplication, \
     QPushButton, \
     QWidget, \
     QLabel, \
-    QListWidget, QListWidgetItem, QMenu, QGridLayout
+    QListWidget, QListWidgetItem, QMenu, QGridLayout, QDateEdit, QHBoxLayout, QComboBox, QMessageBox
 from PyQt6.QtGui import QPixmap, QAction, QFont
-import sys
 
-from APIKeyDialogWindow import APIKeyDialogWindow
-from QRCodeDialog import QRCodeDialog
 from computer import Computer
 import requests
 from QTChat_copy import Chat_Widget
@@ -42,7 +39,27 @@ def parse_log(hardware_id):
     log_list = list()
     for el in log_list_dict:
         tmp = Log(**el)
-        tmp.parse_data()
+        tmp.parse_time()
+        log_list.append(tmp)
+    return log_list
+
+def parse_logs_with_filters(hardware_id,from_timestamp,to_timestamp,log_type):
+    api_url=''
+    if(log_type == 5):
+        api_url = 'http://afire.tech:5000/api/log?hardware_id=' + hardware_id + '&from=' + str(
+            from_timestamp) + '&to=' + str(to_timestamp) + '&api_key=' + API_KEY
+    else:
+        if(log_type == 4):
+            log_type+=1
+        api_url = 'http://afire.tech:5000/api/log?hardware_id=' + hardware_id + '&from=' + str(
+            from_timestamp) + '&to=' + str(to_timestamp) + '&type=' + str(log_type) + '&api_key=' + API_KEY
+    print(api_url)
+    log_list_json = requests.get(api_url)
+    log_list_dict = log_list_json.json()
+    log_list_dict = log_list_dict['logs']
+    log_list = list()
+    for el in log_list_dict:
+        tmp = Log(**el)
         tmp.parse_time()
         log_list.append(tmp)
     return log_list
@@ -51,10 +68,10 @@ def parse_log(hardware_id):
 class ComputerInfoWindow(QMainWindow):
     def __init__(self, hardware_id, parent=None):
         super().__init__(parent)
+        self.log_type = None
         fill_api_key()
         self.comp_info = parse_from_json(hardware_id)
         self.logs = parse_log(hardware_id)
-        print(self.logs)
         self.initUI()
         self._createMenuBar()
         self.index = 0
@@ -89,17 +106,46 @@ class ComputerInfoWindow(QMainWindow):
         for el in self.comp_info.gpus:
             gpus_list.addItem(el)
 
-
-        #temp_chat = QLabel('There will be chat soon')
         chat_class = Chat_Widget()
         chat = chat_class.initLayout()
-        #temp_logs = QLabel('There will be logs soon')
+
+        from_label = QLabel('From')
+        from_label.setMaximumSize(50, 20)
+        to_label = QLabel('To')
+        to_label.setMaximumSize(40, 20)
+        self.from_time_widget = QDateEdit()
+        self.to_time_widget = QDateEdit()
+        self.to_time_widget.setDate(datetime.today())
+        self.from_time_widget.setDate(datetime.today())
+        self.to_time_widget.setMaximumDate(datetime.today())
+        self.from_time_widget.setMaximumDate(datetime.today())
+        self.from_time_widget.setMinimumDate(datetime.fromtimestamp(0))
+        self.to_time_widget.setMinimumDate(datetime.fromtimestamp(0))
+        type_label = QLabel('Log Type')
+        type_label.setMaximumSize(70, 20)
+        self.type_filter = QComboBox()
+        self.type_filter.addItems(['PROCESS', 'LOAD', 'USB_DEVICE', 'NEW_CONFIGURATION', 'BROWSER', 'ALL'])
+        self.type_filter.currentIndexChanged.connect(self.GetType)
+        filter_button = QPushButton('Apply filter')
+        filter_button.clicked.connect(self.filter)
+
+        time_layout = QHBoxLayout()
+        time_layout.setSpacing(5)
+        time_layout.addWidget(from_label)
+        time_layout.addWidget(self.from_time_widget)
+        time_layout.addWidget(to_label)
+        time_layout.addWidget(self.to_time_widget)
+        time_layout.addWidget(type_label)
+        time_layout.addWidget(self.type_filter)
+        time_layout.addWidget(filter_button)
+        self.logs = parse_log(self.comp_info.hardware_id)
         self.CreateLogListWidget()
+
         temp_chart = QLabel('There will be charts soon')
 
-
-        self.grid.addWidget(self.logListWidget, 0, 0, 5, 2)
-        self.grid.addLayout(chat, 6, 0, 5, 2)
+        self.grid.addWidget(self.logListWidget, 0, 0, 6, 2)
+        self.grid.addLayout(time_layout, 6, 0, 1, 2)
+        self.grid.addLayout(chat, 7, 0, 6, 2)
         self.grid.addWidget(ComputerName, 0, 3, 1, 2)
         self.grid.addWidget(ComputerId, 1, 3, 1, 2)
         self.grid.addWidget(OSInfo, 2, 3, 1, 2)
@@ -134,23 +180,44 @@ class ComputerInfoWindow(QMainWindow):
 
     def CreateLogListWidget(self):
         self.logListWidget = QListWidget()
-        self.logs = parse_log(self.comp_info.hardware_id)
         self.timer = QTimer()
         self.timer.timeout.connect(self.fillLogListWidget)
         self.timer.start(50)
 
     def fillLogListWidget(self):
         if self.index < len(self.logs):
-            LogLineWidget = LogCustomQWidget()
-            LogLineWidget.setText(str(self.logs[self.index].data))
-            LogLineWidget.setTime(str(self.logs[self.index].datetime))
-            LogLineWidget.setIcon(self.logs[self.index].type)
-            LogLineWidget.setId(self.logs[self.index].id)
-            logListWidgetItem = QListWidgetItem(self.logListWidget)
-            logListWidgetItem.setSizeHint(LogLineWidget.sizeHint())
-            self.logListWidget.addItem(logListWidgetItem)
-            self.logListWidget.setItemWidget(logListWidgetItem, LogLineWidget)
-            self.index += 1
+            if self.logs[self.index].type == 1:
+                self.index += 1
+            else:
+                LogLineWidget = LogCustomQWidget()
+                LogLineWidget.setText(str(self.logs[self.index].data))
+                LogLineWidget.setTime(str(self.logs[self.index].datetime))
+                LogLineWidget.setIcon(self.logs[self.index].type)
+                LogLineWidget.setId(self.logs[self.index].id)
+                logListWidgetItem = QListWidgetItem(self.logListWidget)
+                logListWidgetItem.setSizeHint(LogLineWidget.sizeHint())
+                self.logListWidget.addItem(logListWidgetItem)
+                self.logListWidget.setItemWidget(logListWidgetItem, LogLineWidget)
+                self.index += 1
         if self.index >= len(self.logs):
             self.timer.stop()
             self.index = 0
+
+    def filter(self):
+        to_time = int(datetime.timestamp(self.to_time_widget.dateTime().toPyDateTime()))
+        from_time = int(datetime.timestamp(self.from_time_widget.dateTime().toPyDateTime()))
+        if (to_time>=from_time):
+            self.logs = parse_logs_with_filters(self.comp_info.hardware_id, from_time, to_time, self.log_type)
+            self.logListWidget.clear()
+            self.CreateLogListWidget()
+            self.grid.addWidget(self.logListWidget, 0, 0, 6, 2)
+            self.grid.update()
+        else:
+            msg = QMessageBox()
+            msg.setWindowTitle("Filter error")
+            msg.setText("Date setting is invalid")
+            msg.setIcon(QMessageBox.Warning)
+            msg.exec_()
+
+    def GetType(self, type):
+        self.log_type = type
--- main_code/Log.py
+++ main_code/Log.py
@@ -11,18 +11,5 @@ class Log:
     timestamp: int
     data: dict
 
-    def parse_data(self):
-        if(self.type == 0):
-            self.data = self.data["processes"]
-        elif(self.type==1):
-            pass
-        elif(self.type==2):
-            pass
-        elif(self.type==3):
-            pass
-        elif(self.type == 5):
-            self.data = self.data["browser_history"]
-
     def parse_time(self):
         self.datetime = datetime.datetime.fromtimestamp(self.timestamp)
-        print(self.datetime)
